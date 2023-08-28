a6ce1d9e6f749688cfbe7db07d9aff1cd114ab6a
CalebNdumbu <138727654+CalebNdumbu@users.noreply.github.com>
2023-08-29 00:08:58 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,36 @@
 #include "main.h"
-
 /**
 * _strstr - function locate
 * @haystack: pointer to char
 * @needle: pointer to char
 * Return: 0
 */
-
 char *_strstr(char *haystack, char *needle)
 {
-	char *result = haystack, *fneedle = needle;
+        char *result = haystack;
+        char *pneedle = needle;
+    
 
-	while (*haystack)
-	{
-		while (*needle)
-		{
-			if (*haystack++ != *needle++)
-			{
-				break;
-			}
-		}
-		if (!*needle)
-		{
-			return (result);
-		}
-		needle = fneedle;
-		result++;
-		haystack = result;
-	}
-	return (0);
+            while(*haystack)
+            {
+                while(*needle)
+                {
+                    if(*haystack != *needle)
+                    {
+                        break;
+                    }   
+            
+                    *haystack++;
+                    *needle++;
+                }
+                if(!*needle)
+                {
+                        return(result);
+                }
+        
+                needle = pneedle;
+                result++;
+                haystack = result;
+            }
+            return (0);
 }
