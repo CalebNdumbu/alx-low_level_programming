ae9d2922f17792438e691a8df91d24acae141f54
CalebNdumbu <calebkyamaweb@gmail.com>
2023-08-28 12:31:24 -0700

    solved 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Locates occurrence in the s of any bytes in the accept
+ * @s: source string
+ * @accept: searching string
+ * 
+ * Return: new string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+    int i;
+
+    while(*s)
+    {
+        for(i=0;accept[i];i++)
+        {
+            if(*s==accept[i])
+            {
+                return(s);
+            }
+        }
+        s++;
+    }
+    return (NULL);
+}
+
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}
+
+
