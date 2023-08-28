b30fcdb743d0a39fac9b4b4ae3878fa1bfd6d98f
CalebNdumbu <138727654+CalebNdumbu@users.noreply.github.com>
2023-08-28 22:50:11 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _strpbrk - bytes
- * @s: source string
- * @accept: searching string
- * 
- * Return: new string
+ * _strpbrk - bytes
+ * @s: pointer to char
+ * @accept: pointer to char
+ * Return: NULL
  */
 char *_strpbrk(char *s, char *accept)
 {
