32249582c8bb80dd5c61fadc064febc4870e1686
CalebNdumbu <calebkyamaweb@gmail.com>
2023-08-28 08:11:47 -0700

    file that fills memory with a constant byt

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s:s: A pointer to the starting address of memory to be filled.
+ *b: The desired value (a constant byte) to fill the memory with.
+ *n: The number of bytes to be changed.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    for(int i; n > 0; i++)
+    {
+        s[i] = b;
+        n--;
+    }
+    return(s)
+}
