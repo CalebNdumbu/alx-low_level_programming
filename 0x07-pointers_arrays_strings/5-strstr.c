4a7a290d54fb80b73c9fed800e92bd48f07c333f
CalebNdumbu <calebkyamaweb@gmail.com>
2023-08-28 13:49:52 -0700

    solved 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strpbrk - funtion to locate
+ * @s: ptr to char
+ * @accept: ptr to char
+ * 
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    char *result = haystack;
+    char *pneedle = needle;
+    
+
+    while(*haystack)
+    {
+        while(*needle)
+        {
+            if(*haystack != *needle)
+            {
+                break;
+            }
+            
+            *haystack++;
+            *needle++;
+        }
+        if(!*needle)
+        {
+            return(result);
+        }
+        
+        needle = pneedle;
+        result++;
+        haystack = result;
+    }
+    return (0);
+}
+
