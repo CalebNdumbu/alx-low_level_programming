d44c8031eb9051269a6344bd9dfdfbea7ae55903
CalebNdumbu <138727654+CalebNdumbu@users.noreply.github.com>
2023-08-29 00:24:41 +0300

    edit in betty

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,15 +9,12 @@
  {
         int i,j;
     
-     
         for(i = 0; i < 8; i++)
         {
             for(j = 0; j < 8; j++)
             {
                 _putchar(*(*(i + a) + j));
-            }
-         
+            }         
         _putchar('\n');
         }
  }
-
