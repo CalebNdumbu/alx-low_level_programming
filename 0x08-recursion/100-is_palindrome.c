bfc876f8997665d3e4aef03db6d0f1b5d90abd63
CalebNdumbu <138727654+CalebNdumbu@users.noreply.github.com>
2023-08-31 00:48:27 +0300

    fixing bugs

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,50 +1,44 @@
-#include "main.h"
+i#include "holberton.h"
 
 /**
- * palind - calculate length of string
+ * palind2 - obtains length of a
  * @a: string
- * @l: int to count length
+ * @l: integer to count length
  *
- * Return: 1 on success
+ * Return: On success 1.
  */
-
-int palind(char *a, int l)
+int palind2(char *a, int l)
 {
 	if (*a == 0)
 		return (l - 1);
-
-	return (palind(a + 1, l + 1));
+	return (palind2(a + 1, l + 1));
 }
-
 /**
- * palind2 - compare string to its reverse
+ * palind3 - compares string vs string reverse
  * @a: string
  * @l: length
  *
- * Return: 1 on success
+ * Return: On success 1.
  */
 
-int palind2(char *a, int l)
+int palind3(char *a, int l)
 {
-	if (*a != *(a + 1))
+	if (*a != *(a + l))
 		return (0);
-
 	else if (*a == 0)
 		return (1);
-
-	return (palind2(a + 1,l - 2));
+	return (palind3(a + 1, l - 2));
 }
-
 /**
- * is_palindrome -  checks if a string is a palindrome
+ * is_palindrome - checks if a string is a palindrome
  * @s: string to evaluate
  *
- * Return: 1 on success
+ * Return: On success 1.
  */
-
 int is_palindrome(char *s)
 {
 	int l;
-	l = palind(s, 0);
-	return (palind2(s, l));
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
