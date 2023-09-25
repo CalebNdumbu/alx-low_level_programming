4070f300fd6b49a0ae41fc785e6b7a90f9632980
CalebNdumbu <calebkyamaweb@gmail.com>
2023-09-25 14:43:00 -0700

    done 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+/**
+ * listint_len -  returns the number of elements in a linked listint_t list
+ * @h: linked list
+ *
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+    size_t number = 0;
+    const listint_t *ptr = h;
+
+    while (ptr)
+    {
+        number++;
+        ptr = ptr->next;
+    }
+
+    return (number);
+}
