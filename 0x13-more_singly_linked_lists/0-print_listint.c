98db43bb1f15f4b954eeadb157124f83654ca1d1
CalebNdumbu <calebkyamaweb@gmail.com>
2023-09-25 14:22:56 -0700

    corrected 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,13 +13,9 @@ size_t print_listint(const listint_t *h)
 
     const listint_t *ptr = h;
 
-    while (ptr != NULL)
+    while (ptr)
     {
-<<<<<<< HEAD
         printf("%d\n", ptr->n);
-=======
-        printf("%d\n, ", ptr->n);
->>>>>>> 465021a63000f6598e6c8f86e4f67b1b5d7f04fb
         number++;
         ptr = ptr->next;
     }
