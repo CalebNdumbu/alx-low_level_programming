56b7dfba0938bee604bfaecb4ec1af5399289342
CalebNdumbu <calebkyamaweb@gmail.com>
2023-09-25 17:38:01 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
     unsigned int i;
     listint_t *new_node;
+    listint_t *temp = *head;
 
     new_node = malloc(sizeof(listint_t));
     if (!new_node || !head)
@@ -27,7 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
         return (new_node);
     }
 
-    listint_t *temp = *head;
     for (i = 0; temp && i < idx; i++)
     {
         if (i == idx - 1)
