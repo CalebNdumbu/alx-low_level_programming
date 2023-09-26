7bc7552d453b4a0d8e0f43d9b417d5e6e2604af0
CalebNdumbu <calebkyamaweb@gmail.com>
2023-09-26 07:35:49 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,15 +25,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
     else
     {
 
-        while (index != 0)
+        while (i < index - 1)
         {
-            temp = current;
-            current = current->next;
-            index--;
+            if (!temp || !(temp->next))
+                return (-1);
+            temp = temp->next;
+            i++;
         }
 
+        current = temp->next;
         temp->next = current->next;
         free(current);
+
         return (1);
     }
 }
